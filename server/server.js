4965980263bf7e295cabad6512ebe91fd5d88f5d
snowbytes <84378360+snowbytes@users.noreply.github.com>
2025-02-23 23:27:27 +0000

    connect to an atlas cluster as a cloud db

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import { createServer } from "node:http";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
@@ -19,12 +20,26 @@ if (process.env.NODE_ENV == "production") {
   });
 }
 
+const DB_URI = process.env.DB_URI;
+if (DB_URI === undefined) {
+  throw new Error(
+    "error finding DB_URI in env vars. setup .env.local following the documentation inside .env",
+  );
+}
+
 const PORT = process.env.PORT;
 if (PORT === undefined) {
   throw new Error(
     "error finding PORT in env vars. setup .env.local following the documentation inside .env",
   );
 }
+
+await mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.info("connection with database successfully established");
+  })
+  .catch(console.error);
 
 createServer(app).listen(PORT, () => {
   console.info(`server has started listening on port ${PORT}`);
